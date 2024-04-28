1242ee2143e2b43045f218136dcdb5758e49ff37
quguiyou <quguiyou1076@qq.com>
2024-04-28 21:41:27 +0800

    初始化项目

---
+++ rvld.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+	"rvld/pkg/linker"
+	"rvld/pkg/utils"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		utils.Fatal("wrong args")
+	}
+
+	file := linker.MustNewFile(os.Args[1])
+
+	if !linker.CheckMagic(file.Contents) {
+		utils.Fatal("not an ELF file")
+	}
+}
